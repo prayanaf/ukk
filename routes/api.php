ec9938cd585c95f7218d6f17b7d91e3fcfd775f9
sabian77 <sabianrakasinau@gmail.com>
2025-05-26 05:36:38 +0700

    add file for api

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,16 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\SiswaController;
+use App\Http\Controllers\Api\IndustriController;
+use App\Http\Controllers\Api\PklController;
 use App\Http\Controllers\Api\GuruController;
 
-Route::apiResource('gurus', GuruController::class);
+Route::apiResource('guru', GuruController::class);
+Route::apiResource('siswa', SiswaController::class);
+Route::apiResource('industris', IndustriController::class);
+Route::apiResource('pkl', PklController::class);
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
